69ecbe9513f5b243c58afbffce5798307614ab09
dlazocaasa <dlazo@acerosarequipa.com>
2025-01-03 13:47:27 -0500

    algo mas

--- eda_plots1.py
+++ eda_plots1.py
@@ -2,5 +2,13 @@
 EDA Refractarios
 """
 import pandas as pd
+import os
 
-## esto es una prueba
+# Working directory
+os.chdir('C:\\proyectos 2025\\refractarios')
+
+# load data
+df0=pd.read_csv('prod_2324.csv')
+
+# EDA
+df0.head()
